6bce0c42fe45881fdfd189e992eb4b39370d5230
vishnuacappella <acappella.studiov@gmail.com>
2022-07-10 05:32:15 +0530

    index.html and script file created

---
+++ script.js
@@ -0,0 +1,82 @@
+// console.log('Hello World');
+// var name = 'vishnu';
+// console.log(name);
+
+// var schoolTeam = 'Football';
+// var schoolNumber = 20;
+// !Boolean
+// var isRegistered = true;
+// console.log(isRegistered);
+
+//! UNDEFINED
+// var job;
+// console.log(job);
+
+// job = 'Student'
+// console.log(job);
+
+//! Type Coercion
+
+// console.log(name + ' ' + schoolNumber);
+// var schoolName, hasCourse;
+// schoolName = 'Demo School';
+// hasCourse = true;
+
+// console.log(schoolName + ' has 35 courses. Does it have any course ? ' + hasCourse);
+
+// !Mutation
+
+// console.log(schoolName + ' has 35 courses. Does it have any course ? ' + hasCourse);
+
+/*console.log(name + ' ' + schoolNumber);
+var schoolName, hasCourse;
+schoolName = 'Demo School';
+hasCourse = true;
+*/
+
+// schoolNumber = 'twenty two';
+// console.log(schoolNumber);
+
+// schoolTeam = "Cricket";
+// console.log(schoolTeam);
+
+// !alert
+// alert(schoolTeam);
+
+// !prompt
+// var age = prompt('how old are you ?')
+// console.log(`age: ${age}`);
+// console.log('Alex is ' + age + ' years old');
+
+// !!!!!!!!!!!!!!!!!!!!! BASIC OPERATORS !!!!!!!!!!!!!!!!!!!!!
+
+var totalDistance, distanceAlex, distanceSam;
+
+totalDistance = 20;
+distanceAlex = totalDistance - 12;
+console.log(distanceAlex);
+
+distanceSam = totalDistance - 7;
+console.log(distanceSam);
+
+console.log(distanceSam * 2);
+
+console.log(distanceSam / 2);
+
+// !Logical Operators
+
+var distanceToSchool, distanceToHome;
+
+distanceToHome = 14;
+distanceToSchool = 6;
+
+var isCloser = distanceToSchool > distanceToHome;
+console.log(isCloser);
+
+// !Boolean Logic
+
+var a = '10';
+var b = 10;
+
+console.log(a == b);
+console.log(a === b);
